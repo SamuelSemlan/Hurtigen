d19d68ed8afea204b2e15d2218f410dddcd71ff6
SamuelSemlan <semlan12@live.com>
2017-02-08 22:54:59 +0100

    Minor fixes

--- hurtigen.py
+++ hurtigen.py
@@ -11,6 +11,10 @@ def home():
 def about():
 	return render_template("about.html")
 
+@app.route("/profile/")
+def profile():
+	return render_template("profile.html")
+
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
 
