ede2ec2785ad49fb96780103343efcbb15b2663b
SamuelSemlan <semlan12@live.com>
2017-02-14 09:06:30 +0100

    Added HTML

--- hurtigen.py
+++ hurtigen.py
@@ -15,6 +15,10 @@ def about():
 def profile():
 	return render_template("profile.html")
 
+@app.route("/settings/")
+def settings():
+	return render_template("settings.html")
+
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
 
