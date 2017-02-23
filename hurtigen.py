441b0c8c4f26f30ddbdd12b2f16b935014f01aba
SamuelSemlan <semlan12@live.com>
2017-02-23 13:59:10 +0100

    Added form in contact.html

--- hurtigen.py
+++ hurtigen.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("hurtigen")
+
 
 @app.route("/")
 def home():
 	return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-	return render_template("about.html")
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        page = "You are {} with the email {} and your message was {}".format(
+            request.form["name"],
+            request.form["email"],
+            request.form["message"]
+        )
+        return page
+    else:
+        return render_template("contact.html")
 
-@app.route("/profile/")
+@app.route("/profile")
 def profile():
 	return render_template("profile.html")
 
-@app.route("/settings/")
+@app.route("/settings")
 def settings():
 	return render_template("settings.html")
+
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
