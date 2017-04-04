67f1c1496d50359896504d69014ac4fff01c4c55
SamuelSemlan <semlan12@live.com>
2017-04-04 08:53:16 +0200

    Fixed contact page

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators	
+from wtforms import StringField, TextAreaField, validators	
 
 class MyForm(FlaskForm):
 	name = StringField("Name", [validators.InputRequired()])
-	
+	message = TextAreaField("Message", [validators.InputRequired()])
--- hurtigen.py
+++ hurtigen.py
@@ -24,7 +24,7 @@ def home():
 def contact():
     form = MyForm()
     if form.validate_on_submit():
-        return "Hey there, {}!".format(form.name.data)
+        return "Thanks for submitting, {}!".format(form.name.data)
     else:
         return render_template("contact.html", form=form)
 
